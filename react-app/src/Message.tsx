f3a49c52947e87a3cc78447c2e9facfbbe5dd897
stana <YOUR_EMAIL>
2025-06-11 21:53:29 +0200

    react

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+ import React from "react";
+ //pascal casing
+function Message() {
+//jsx: javaScript XML
+const name ="";
+if (name)
+return <h1>Hello {name}</h1>    
+return <h1>Hello the world </h1>;
+}
+export default Message;
